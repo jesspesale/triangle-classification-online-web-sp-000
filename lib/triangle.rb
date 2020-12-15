08d0955fc2f452d4b525f8cb273d2fd8313d1ecb
Jess Pesale <jess_pesale@aol.com>
2020-12-15 17:30:43 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,26 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :x, :y, :z
+  
+  def initialize(x, y, z)
+    @x = x 
+    @y = y 
+    @z = z 
+  end
+  
+  def kind
+    if x == y && y == z
+      :equilateral
+    elsif x == y || y == z || x == z
+       :isosceles
+    else
+      :scalene
+  end
+  end
+  
 end
+
+    # elsif x + y > z || x + z > y || y + z > x  
+  # else
+  #   puts "theres an error"
+  # end
